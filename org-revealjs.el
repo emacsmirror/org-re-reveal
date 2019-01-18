c6af3bb2fd7b6b7856aa316429549843a9748557
Jens Lechtenbörger <lechten@wi.uni-muenster.de>
2019-01-18 18:29:10 +0100

    Add purpose and history to commentary

--- org-revealjs.el
+++ org-revealjs.el
@@ -30,6 +30,17 @@
 ;; Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
+;; This package provides Org export functionality to generate HTML
+;; presentations with the presentation framework reveal.js.
+;; It grew out of a forked version of org-reveal whose development
+;; seems to have stalled and which cannot be installed with recent
+;; versions of Org mode any more:
+;; https://github.com/yjwen/org-reveal/issues/342
+;; I tried to contribute to org-reveal but did not receive a response
+;; from the maintainer:
+;; https://github.com/yjwen/org-reveal/issues/349
+;; https://github.com/yjwen/org-reveal/pull/277
+;;
 ;; See "Readme.org" for introduction and details.
 
 ;;; Code:
