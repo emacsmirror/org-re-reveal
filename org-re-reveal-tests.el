f7e2fff8005703b72e2fc6114c7a0ae205e0ea47
conao3 <conao3@gmail.com>
2019-04-29 00:26:43 +0900

    fix org-re-reveal-tests-get-file-contents

--- org-re-reveal-tests.el
+++ org-re-reveal-tests.el
@@ -62,9 +62,9 @@
 (defun org-re-reveal-tests-get-file-contents (name &optional folder)
   "Get file named NAME contents in FOLDER."
   (with-temp-buffer
-    (insert-file-contents (format "%s/%s"
-                                  (or load-file-name default-directory) name)
-                          (or folder "test-cases"))
+    (insert-file-contents
+     (expand-file-name (format "%s/%s" (or folder "test-cases") name)
+                       (org-re-reveal-f-this-file-dir)))
     (buffer-substring (point-min) (point-max))))
 
 (cort-deftest org-re-reveal/cort-test
