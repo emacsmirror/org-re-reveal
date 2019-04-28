54d2c30a1f2320be4196d3508bad4de495c65dc6
conao3 <conao3@gmail.com>
2019-04-29 00:01:39 +0900

    define highlight.js test case

--- org-re-reveal-tests.el
+++ org-re-reveal-tests.el
@@ -39,17 +39,24 @@
 (require 'cort-test)
 (require 'org-re-reveal)
 
-;; (defun org-re-reveal-tests-get-file-contents (name &optional folder)
-;;   "Get file named NAME contents in FOLDER."
-;;   (with-temp-buffer
-;;     (insert-file-contents (format "%s/%s"
-;;                                   (or load-file-name default-directory) name)
-;;                           (or folder "test-cases"))
-;;     (buffer-substring (point-min) (point-max))))
+(defun org-re-reveal-tests-get-file-contents (name &optional folder)
+  "Get file named NAME contents in FOLDER."
+  (with-temp-buffer
+    (insert-file-contents (format "%s/%s"
+                                  (or load-file-name default-directory) name)
+                          (or folder "test-cases"))
+    (buffer-substring (point-min) (point-max))))
 
 (cort-deftest org-re-reveal/cort-test
   '((:string= "https://gitlab.com/oer/org-re-reveal"
               "https://gitlab.com/oer/org-re-reveal")))
+
+(cort-deftest org-re-reveal/highlightjs
+  '((org-re-reveal-tests-get-file-contents "test-highlightjs.html")
+    (progn
+      ;; TODO: create expect-highlightjs.html
+      (org-re-reveal-tests-get-file-contents "expect-highlightjs.html"))))
+
 
 (provide 'org-re-reveal-tests)
 ;;; org-re-reveal-tests.el ends here
