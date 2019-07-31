cdab7b587c524614a9bc46b30eb3f582103371c6
Jens Lechtenbörger <lechten@wi.uni-muenster.de>
2019-07-31 11:24:58 +0200

    Version 1.1.7

--- org-re-reveal.el
+++ org-re-reveal.el
@@ -8,7 +8,7 @@
 ;; Copyright (C) 2019      Ayush Goyal <perfectayush@gmail.com>
 
 ;; URL: https://gitlab.com/oer/org-re-reveal
-;; Version: 1.1.6
+;; Version: 1.1.7
 ;; Package-Requires: ((emacs "24.4") (org "8.3") (htmlize "1.34"))
 ;; Keywords: tools, outlines, hypermedia, slideshow, presentation, OER
 
