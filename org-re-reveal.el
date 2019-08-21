ef116cb4a7fd24ae2f883b4566195ddca4e948fe
Jens Lechtenbörger <lechten@wi.uni-muenster.de>
2019-08-21 08:26:59 +0200

    Fix version number
    Switch to semantic versioning.
    This closes issue #24 (if applied consistently).

--- org-re-reveal.el
+++ org-re-reveal.el
@@ -8,7 +8,7 @@
 ;; Copyright (C) 2019      Ayush Goyal <perfectayush@gmail.com>
 
 ;; URL: https://gitlab.com/oer/org-re-reveal
-;; Version: 1.1.11
+;; Version: 2.0.0
 ;; Package-Requires: ((emacs "24.4") (org "8.3") (htmlize "1.34"))
 ;; Keywords: tools, outlines, hypermedia, slideshow, presentation, OER
 
